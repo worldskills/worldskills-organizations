040b90fc6d8e661431803cf1044859695d73c6f8
Adam Walsh <adam@grummen.net>
2015-11-07 00:03:57 +1000

    tweaks for new worldskills utils

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -8,7 +8,7 @@
     var wsApp = angular.module('orgApp'); 
     wsApp.constant('REST_BASE_URL', restBaseUrl);
     wsApp.constant('CLIENT_ID', '01f623251c02');
-    wsApp.constant('WORLDSKILLS_CLIENT_ID', 'a95703d1aa96');
+    wsApp.constant('WORLDSKILLS_CLIENT_ID', '01f623251c02');
     wsApp.constant('API_AUTH', authBaseUrl + '/auth');
     wsApp.constant('WORLDSKILLS_API_AUTH', authBaseUrl + '/auth');
     wsApp.constant('AUTHORIZE_URL', loginApp + '/oauth/authorize');
@@ -17,7 +17,8 @@
     wsApp.constant('API_AUTH_CODE', 800);
     wsApp.constant('API_IMAGES', 'http://localhost:9292/images/');
     wsApp.constant('API_PEOPLE', 'http://localhost:8080/people');
-    wsApp.constant('LOAD_CHILD_ENTITY_ROLES', true); 
+    wsApp.constant('LOAD_CHILD_ENTITY_ROLES', true);
+    wsApp.constant('FILTER_AUTH_ROLES', [800, 600]);
     
     wsApp.constant('APP_ROLES', {
         ADMIN: 'Admin',
