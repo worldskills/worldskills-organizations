7803add72cccfc2443dd553de7d36f655062a27d
waseem.sabjee <waseem.sabjee@worldskills.org>
2021-03-23 10:35:36 +0200

    update prod client id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   worldskillsApiOrg: 'https://api.worldskills.org/org',
   worldskillsApiPeople: 'https://api.worldskills.org/people',
   worldskillsApiImages: 'https://api.worldskills.org/images',
-  worldskillsClientId: '49775efbb906',
+  worldskillsClientId: '01f623251c02',
   worldskillsAuthorizeUrl: 'https://auth.worldskills.org/oauth/authorize',
   worldskillsAuthorizeRedirect: 'https://org.worldskills.org/',
   worldskillsAuthorizeUserinfoEndpoint: 'https://auth.worldskills.org/auth/users/loggedIn',
