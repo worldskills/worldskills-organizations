1e1864c2ca7d76fc74694736fcc9fd122b8281c1
waseem.sabjee <waseem.sabjee@worldskills.org>
2021-05-20 11:06:40 +0200

    Autologin on logout + added logged out detection

--- src/services/http-interceptor/http-interceptor.service.ts
+++ src/services/http-interceptor/http-interceptor.service.ts
@@ -1,16 +1,18 @@
-import {Injectable, Injector} from '@angular/core';
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {Router} from '@angular/router';
+import { Injectable, Injector } from '@angular/core';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 import {
   AlertService,
   AlertType,
   AuthService,
   NgAuthService,
-  I18nUtil
+  I18nUtil,
+  HttpUtil
 } from '@worldskills/worldskills-angular-lib';
-import {LocaleContextService} from '../locale-context/locale-context.service';
+import { LocaleContextService } from '../locale-context/locale-context.service';
+import { notLoggedInCode, authorizationMissingCode } from '../../app/app-config';
 
 @Injectable({
   providedIn: 'root'
@@ -46,34 +48,70 @@ export class HttpInterceptorService implements HttpInterceptor {
     }
   }
 
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const l = this.overrideLanguage ? this.overrideLanguage : this.currentLanguage;
-    return next.handle(req.clone({
-      body: req.body ? I18nUtil.setObjectI18n(req.body, l) : undefined,
+    const isLogout = request.url.includes('logout');
+    const handled = isLogout ? request : request.clone({
+      body: request.body ? I18nUtil.setObjectI18n(request.body, l) : undefined,
       setParams: {l},
-    })).pipe(tap({
-      error: (event: HttpErrorResponse) => {
-        if (!req.url.startsWith('/assets/') && !HttpInterceptorService.ignoredPaths.includes(req.url)) {
-          if (event.status === 404) {
-            this.router.navigateByUrl('/not-found', {skipLocationChange: true});
-          } else if (event.status === 403) {
-            this.ngAuthService.login();
-          } else if (event.status === 400) {
-            this.alertService.setAlert('request', AlertType.error, event.error,
-              'user_msg' in event.error ? event.error.user_msg : event.message, true);
-          } else {
-            if (event.error && 'user_msg' in event.error) {
-              this.alertService.setAlert('request', AlertType.error, event.error,
-                'user_msg' in event.error ? event.error.user_msg : event.message, true);
-            } else {
-              this.alertService.setAlert('request', event.error, event.statusText, event.message);
-            }
-          }
-        }
-      }
-    }));
+    });
+    return next.handle(handled).pipe(
+      catchError(err => this.interceptError(request, next, err))
+    );
   }
 
+  interceptError(request: HttpRequest<any>, next: HttpHandler, err: any) {
+    if (err instanceof HttpErrorResponse) {
+      // force user to the login page when not logged in or token is expired
+      if (request.url.includes('auth/users/loggedIn')) {
+        if (HttpUtil.isNotLoggedIn(err, notLoggedInCode) || HttpUtil.hasStaleToken(err, authorizationMissingCode)) {
+          sessionStorage.clear();
+          this.ngAuthService.login();
+          return;
+        }
+      }
 
+      switch (err.status) {
+        case 400:
+          // when the logout call fails, clear session then kick to login screen
+          if (request.url.match('logout')) {
+            sessionStorage.clear();
+            this.ngAuthService.login();
+            return;
+          } else {
+            this.alertService.setAlert(
+              'request',
+              AlertType.error, err.error,
+              'user_msg' in err.error ? err.error.user_msg : err.message,
+              true
+            );
+          }
+          break;
+        case 401:
+          if (!request.url.includes('ping')) {
+            if (HttpUtil.isNotLoggedIn(err, notLoggedInCode) || HttpUtil.hasStaleToken(err, authorizationMissingCode)) {
+              sessionStorage.clear();
+              this.ngAuthService.login();
+              return;
+            } else {
+              this.authService.ping().subscribe();
+            }
+          } else {
+            this.ngAuthService.login();
+          }
+          break;
+        default:
+          if (err.error && 'user_msg' in err.error) {
+            this.alertService.setAlert('request', AlertType.error, err.error,
+              'user_msg' in err.error ? err.error.user_msg : err.message, true);
+          } else {
+            this.alertService.setAlert('request', err.error, err.statusText, err.message);
+          }
+          break;
+      }
+    }
+
+    return throwError(err);
+  }
 }
+
