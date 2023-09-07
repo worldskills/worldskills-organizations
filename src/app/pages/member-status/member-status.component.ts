7438afab5d224fa97bb38e8d6589d9431b7acac5
Waseem <waseemsabjee@gmail.com>
2023-09-07 15:50:48 +0200

    Improve filtering member status page

--- src/app/pages/member-status/member-status.component.ts
+++ src/app/pages/member-status/member-status.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Member, MemberList } from '../../../types/member';
 import { environment } from '../../../environments/environment';
-import { MembersFetchParams, MembersService } from '../../../services/members/members.service';
-
-
+import {
+  MembersFetchParams,
+  MembersService,
+} from '../../../services/members/members.service';
+import { GenericUtil } from '@worldskills/worldskills-angular-lib';
 
 @Component({
   selector: 'app-member-status',
   templateUrl: './member-status.component.html',
-  styleUrls: ['./member-status.component.css']
+  styleUrls: ['./member-status.component.css'],
 })
 export class MemberStatusComponent {
   // used for filter
@@ -18,14 +20,13 @@ export class MemberStatusComponent {
   // used for status list
   memberList: MemberList;
   membersFilter: MembersFetchParams;
+  member_of: number;
 
   loading = false;
   exporting = false;
   appId = environment.worldskillsAppId;
 
-  constructor(private members: MembersService) {
-
-  }
+  constructor(private members: MembersService) {}
 
   ngOnInit(): void {
     this.allMembersFilter = {
@@ -40,34 +41,47 @@ export class MemberStatusComponent {
       offset: 0,
       editable: false,
       sort: 'code',
-      member_of: 1 // default WSI
+      member_of: 1, // default WSI
     };
+
+    this.member_of = 1;
 
     this.loading = true;
     this.members.getMemberList(this.allMembersFilter).subscribe(
       (allMembers) => {
         this.allMembers = allMembers;
       },
-      error => this.loading = false,
+      (error) => (this.loading = false),
       () => this.loadMemberList()
     );
   }
 
   loadMemberList() {
+    this.memberList = null;
     this.loading = true;
-    this.members.getMemberList(this.membersFilter).subscribe(
-      (memberList) => {
+    console.log(this.membersFilter);
+    this.members.getMemberList(this.membersFilter).subscribe((memberList) => {
+      this.member_of = this.membersFilter.member_of;
       this.memberList = memberList;
-      this.loading = false;}
-    );
+      this.loading = false;
+    });
   }
 
   getJoinedYear(member: Member) {
-    return member.member_of.find(m => m.id === this.membersFilter.member_of).year_joined;
+    if (GenericUtil.isNullOrUndefined(member)) return null;
+    const found = member.member_of.find(
+      (m) => m.id === this.member_of
+    );
+    if (GenericUtil.isNullOrUndefined(found)) return null;
+    return found.year_joined;
   }
 
   getMemberStatus(member: Member) {
-    return member.member_of.find(m => m.id === this.membersFilter.member_of).status;
+    if (GenericUtil.isNullOrUndefined(member)) return null;
+    const found = member.member_of.find(
+      (m) => m.id === this.member_of
+    );
+    if (GenericUtil.isNullOrUndefined(found)) return null;
+    return found.status;
   }
-
 }
